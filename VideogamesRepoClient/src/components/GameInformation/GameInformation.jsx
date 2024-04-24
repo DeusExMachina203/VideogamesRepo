62e4769a7a4542f7ad1372b344ef0d1bc0bcf8c8
DeusExMachina <ernestodeloayza@gmail.com>
2024-04-24 17:26:12 -0500

    Proportions for desktop view better

--- VideogamesRepoClient/src/components/GameInformation/GameInformation.jsx
+++ VideogamesRepoClient/src/components/GameInformation/GameInformation.jsx
@@ -4,14 +4,16 @@ import ListDisplayer from '../ListDisplayer/ListDisplayer.jsx';
 
 const GameInformation = ({origin, img, name, genres, description, launch_date, rating, platforms}) => {
 
-	console.log(platforms)
 
 	return (
 		<>
 
 			<div className={style.heading}>
-				<div className={style.title}>
-					<h1 className={style.game_name}>{name}</h1>
+				<div className={style.text}>
+					<div className={style.title}>
+						<h1 className={style.game_name}>{name}</h1>
+					</div>
+
 					<div className= {style.game_information}>
 						<ListDisplayer className = {style.tags} elements={genres.split(', ').join('%')} name="Géneros" />
 						<label className = {style.info_label} htmlFor = "launch_date">Fecha de lanzamiento:</label>
@@ -19,17 +21,16 @@ const GameInformation = ({origin, img, name, genres, description, launch_date, r
 						<label className = {style.info_label} htmlFor = "Rating">Rating:</label>
 						<span className = {style.info_bit}>{rating} </span>
 						<label className = {style.info_label} htmlFor = "platforms">Plataformas:</label>
-						<ListDisplayer className = {style.tags}  elements={'thisisawayoflife'.split(' ').join('%')}s name=""/>
+						<ListDisplayer className = {style.tags}  elements={platforms.split(' ').join('%')}s name=""/>
 						<label className = {style.info_label} htmlFor = "description">Descripción:</label>
 						<p dangerouslySetInnerHTML={{__html: description}} />
 						
 						
 					</div>
-					
 				</div>
 				<img src={img?img:ghost} className={style.game_background_image} alt="game image" />
-				
 			</div>
+			
 		</>
 	)
 };
